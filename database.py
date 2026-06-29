5d25f0749e01c5a890c18d97b45a654b8e5a51df
sayalisananse101080-ops <sayalisananse101080@gmail.com>
2026-06-29 14:37:09 +0530

    added new files and updated project

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
